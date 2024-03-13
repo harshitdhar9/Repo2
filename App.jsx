fc786bd28c2489da0410c66476467c538f779c9e
HARSHIT DHAR <143949444+harshitdhar9@users.noreply.github.com>
2024-03-13 15:12:49 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,12 +1,10 @@
-import Header from "./Header.jsx"
-import Footer from "./Footer.jsx"
-
-function App(){
-  return (
+import Card from './Card.jsx'
+function App() {
+  return(
     <>
-    <Header/>
-    <Footer/>
+    <Card/>
     </>
-  )
+  );
 }
+
 export default App
