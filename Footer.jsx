aaf140276936fb4806016a48cc8174576b4d6617
HARSHIT DHAR <143949444+harshitdhar9@users.noreply.github.com>
2024-03-13 15:10:23 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer className="Footer">
+            <p>&copy; {new Date().getFullYear()} Fruitify fruit@gmail.com</p>
+        </footer>
+    );
+}
+export default Footer
---
+++ Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+    return(
+        <Header>
+        <h2>Fruitify</h2>
+        <h3 className="Title">Website for Fruits</h3>
+        <nav>
+            <ul>
+                <li className="List">Home<a href="#"></a></li>
+                <li className="List">Types<a href="#"></a></li>
+                <li className="List">Origin<a href="#"></a></li>
+            </ul>
+        </nav>
+        </Header>
+    );
+}
+export default Header
